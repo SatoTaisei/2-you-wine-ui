83431a9d1dcc6dc06ee9a9c3df973c652daeb5db
Sato Taisei <t.sei43rm@gmail.com>
2026-06-28 00:54:24 +0900

    ⚰️

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { css } from "styled-system/css"
-
 function App() {
-	return (
-		<div className={css({ fontSize: "2xl", fontWeight: "bold" })}>
-			Hello 🐼!
-		</div>
-	)
+	return null
 }
 
 export default App
